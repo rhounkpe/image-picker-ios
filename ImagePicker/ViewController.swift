ae4aae70472a39ad802db04d47843e1286914f1e
Rufin Hounkpe <rhounkpe@gmail.com>
2020-06-18 19:45:12 +0200

    Initial Commit

---
+++ ImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Rufin Hounkpe on 18/06/2020.
+//  Copyright © 2020 Digit Com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
